3038d86bfa733fea2a2786dd92855f4e6ecd8550
Nick-Major <divanchik26@gmail.com>
2024-10-09 23:50:13 +0300

    Third commit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
-export let warrior = {
-    name: 'Воин', 
-    health: 70
-}
+export const warrior = {
+  name: 'Воин',
+  health: 70,
+};
 
-export let magician = {
-    name: 'Маг', 
-    health: 47
-}
+export const magician = {
+  name: 'Маг',
+  health: 47,
+};
 
-export let archer = {
-    name: 'Лучник', 
-    health: 12
-}
+export const archer = {
+  name: 'Лучник',
+  health: 12,
+};
 
 export function characterStatus(character) {
-    if(character.health > 50) {
-        return 'healthy';
-    } else if (character.health <= 50 && character.health >= 15) {
-        return 'wounded';
-    } else if (character.health < 15) {
-        return 'critical';
-    }
+  let healtStatus;
+  if (character.health > 50) {
+    healtStatus = 'healthy';
+  }
+  if (character.health <= 50 && character.health >= 15) {
+    healtStatus = 'wounded';
+  }
+  if (character.health < 15) {
+    healtStatus = 'critical';
+  }
+  return healtStatus;
 }
--- test/test.js
+++ test/test.js
@@ -1,20 +1,18 @@
-import { characterStatus, warrior, magician, archer } from "../src/index.js";
+import {
+  characterStatus, warrior, magician, archer,
+} from '../src/index';
 
-test ('healthy display test', () => {
-    
-    const result = characterStatus(warrior);
-    expect(result).toBe('healthy');
+test('healthy display test', () => {
+  const result = characterStatus(warrior);
+  expect(result).toBe('healthy');
 });
 
-test ('wounded display test', () => {
-    
-    const result = characterStatus(magician);
-    expect(result).toBe('wounded');
+test('wounded display test', () => {
+  const result = characterStatus(magician);
+  expect(result).toBe('wounded');
 });
 
-
-test ('critical display test', () => {
-    
-    const result = characterStatus(archer);
-    expect(result).toBe('critical');
+test('critical display test', () => {
+  const result = characterStatus(archer);
+  expect(result).toBe('critical');
 });
